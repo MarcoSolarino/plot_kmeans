51dfeb6967a4a0febcb61a700cfa9ec6d341ce8d
Simone <simone.pezzulla@stud.unifi.it>
2021-02-17 11:41:34 +0100

    aaa

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import pandas
 
+base_dir = "/Users/simone/CLionProjects/Midterm_Parallel_Computing_K-means/output/"
 
-def plot_k_means_iteration(dataset, centroids):
-    df_dataset = pandas.read_csv(dataset, header=None)
-    df_centroids = pandas.read_csv(centroids, header=None)
+
+def plot_k_means_iteration(k, iteration):
+    df_dataset = pandas.read_csv(base_dir + 'k' + str(k) + 'iteration' + str(iteration) + '.csv', header=None)
+    df_centroids = pandas.read_csv(base_dir + 'k' + str(k) + 'centroids' + str(iteration) + '.csv', header=None)
 
     data0 = df_dataset.loc[df_dataset[3] == 0]
     data1 = df_dataset.loc[df_dataset[3] == 1]
@@ -13,22 +15,9 @@ def plot_k_means_iteration(dataset, centroids):
 
     fig = plt.figure()
 
-    # 3d case
-    # ax = fig.add_subplot(111, projection='3d')
-    #
-    # ax.scatter(data0.iloc[:, 0], data0.iloc[:, 1], data0.iloc[:, 2], c='red')
-    # ax.scatter(df_centroids.iloc[:, 0], df_centroids.iloc[:, 1], df_centroids.iloc[:, 2], marker='*', c='black')
-    # ax.scatter(data1.iloc[:, 0], data1.iloc[:, 1], data1.iloc[:, 2], c='green')
-    # ax.scatter(data2.iloc[:, 0], data2.iloc[:, 1], data2.iloc[:, 2], c='blue')
-    #
-    # ax.set_xlabel('X Axis')
-    # ax.set_ylabel('Y Axis')
-    # ax.set_zlabel('Z Axis')
-
-    # 2d case
-    plt.scatter(data0.iloc[:, 0], data0.iloc[:, 1], c='red')
-    plt.scatter(data1.iloc[:, 0], data1.iloc[:, 1], c='green')
-    plt.scatter(data2.iloc[:, 0], data2.iloc[:, 1], c='blue')
+    plt.scatter(data0.iloc[:, 0], data0.iloc[:, 1])
+    plt.scatter(data1.iloc[:, 0], data1.iloc[:, 1])
+    plt.scatter(data2.iloc[:, 0], data2.iloc[:, 1])
     plt.scatter(df_centroids.iloc[:, 0], df_centroids.iloc[:, 1], marker='*', c='black')
 
     plt.show()
@@ -36,6 +25,4 @@ def plot_k_means_iteration(dataset, centroids):
     return df_dataset
 
 
-for i in range(3):
-    plot_k_means_iteration("D:/JetBrains/Projects/Clion/Kmeans/output/iteration" + str(i) + ".csv", "D:/JetBrains/Projects/Clion/Kmeans/output/centroids" + str(i) + ".csv")
-
+plot_k_means_iteration(5, 1)
