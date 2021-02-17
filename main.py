82d281e6eafc873974973c1484f2c6c24911a7af
Simone <simone.pezzulla@stud.unifi.it>
2021-02-17 11:59:52 +0100

    minor bugfix

--- main.py
+++ main.py
@@ -9,20 +9,18 @@ def plot_k_means_iteration(k, iteration):
     df_dataset = pandas.read_csv(base_dir + 'k' + str(k) + 'iteration' + str(iteration) + '.csv', header=None)
     df_centroids = pandas.read_csv(base_dir + 'k' + str(k) + 'centroids' + str(iteration) + '.csv', header=None)
 
-    data0 = df_dataset.loc[df_dataset[3] == 0]
-    data1 = df_dataset.loc[df_dataset[3] == 1]
-    data2 = df_dataset.loc[df_dataset[3] == 2]
+    cluster = []
+    for i in range(k):
+        cluster.append(df_dataset.loc[df_dataset[3] == i])
 
     fig = plt.figure()
 
-    plt.scatter(data0.iloc[:, 0], data0.iloc[:, 1])
-    plt.scatter(data1.iloc[:, 0], data1.iloc[:, 1])
-    plt.scatter(data2.iloc[:, 0], data2.iloc[:, 1])
+    for i in range(k):
+        plt.scatter(cluster[i].iloc[:, 0], cluster[i].iloc[:, 1])
+
     plt.scatter(df_centroids.iloc[:, 0], df_centroids.iloc[:, 1], marker='*', c='black')
 
     plt.show()
 
-    return df_dataset
 
-
-plot_k_means_iteration(5, 1)
+plot_k_means_iteration(6, 5)
